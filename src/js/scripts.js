6dbc2e9bd7589599a53af5b33882183ea69a73af
LeonardoRamos <leoslramos@gmail.com>
2015-06-04 10:55:40 -0300

    Corrige bug que impedia a troca de imagem para o sol

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,19 +7,19 @@ $(function() {
         $.get('http://sensor-api.localdata.com/api/v1/sources/ci4vye225000n02s7rxjdfxa1/entries.json?count=1&sort=desc')
             .done(function(response) {
                 var currentLight = response.data[0].data.light;
-                var percentageHeight = ((1-(currentLight / LIGHT_33)) * 100).toFixed(0);
-                if(percentageHeight >= 80) {
+                var percentageHeight = ((1 - (currentLight / LIGHT_33)) * 100).toFixed(0);
+                if (percentageHeight >= 80) {
                     percentageHeight = 80;
                 }
-                else if(percentageHeight < 0) {
+                else if (percentageHeight < 0) {
                     percentageHeight = 0;
                 }
 
                 var state = 'img/suncomplete.svg';
-                if(percentageHeight <= LIGHT_13) {
+                if (currentLight <= LIGHT_13) {
                     state = 'img/half.svg';
                 }
-                else if(percentageHeight <= LIGHT_23) {
+                else if (currentLight <= LIGHT_23) {
                     state = 'img/moon.svg';
                 }
 
