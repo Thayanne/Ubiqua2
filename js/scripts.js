e87d4ac0f3173415ef97e3647d2921faa097622d
LeonardoRamos <leoslramos@gmail.com>
2015-06-11 00:19:53 -0300

    Mudando o ednpoint pro endpoint do Rio de Janeiro, pois o endpoint de Niterói estava com bug no sensor de luz, indicava a mesma luz em todos os horários. Refazendo também, o calculo da altura, que agora leva em consideração a altura relativa a luminosidade na faixa, não mais no todo.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
-$(function() {
+$(function () {
     var LIGHT_33 = 6000;
-    var LIGHT_23 = 3000;
-    var LIGHT_13 = 0;
+    var LIGHT_23 = 2000;
+    var LIGHT_13 = 50;
 
     function update() {
-        $.get('http://sensor-api.localdata.com/api/v1/sources/ci4vye225000n02s7rxjdfxa1/entries.json?count=1&sort=desc')
-            .done(function(response) {
+        $.get ('http://sensor-api.localdata.com/api/v1/sources/ci4vjer3i000e02s7r2cj23gs/entries.json?count=1&sort=desc')
+            .done(function (response) {
                 var currentLight = response.data[0].data.light;
-                var percentageHeight = ((1 - (currentLight / LIGHT_33)) * 100).toFixed(0);
-                if (percentageHeight >= 80) {
-                    percentageHeight = 80;
-                }
-                else if (percentageHeight < 0) {
-                    percentageHeight = 0;
-                }
 
-                var state = 'img/suncomplete.svg';
-                if (currentLight <= LIGHT_13) {
-                    state = 'img/half.svg';
+                if (currentLight > LIGHT_23) {
+                    var state = 'img/suncomplete.svg';
+
+                    var totalOfLight = (LIGHT_33 - LIGHT_23);
+                    var sectionLight = (currentLight - LIGHT_23);
+                    var percentageHeight = ((33.3) * ((1 - (sectionLight) / totalOfLight))).toFixed(0);
+                    
+                    if (percentageHeight < 0) {
+                        percentageHeight = 0;
+                    }
+                } 
+                else if (currentLight <= LIGHT_23 && currentLight > LIGHT_13) {
+                    var state = 'img/half.svg';
+
+                    var totalOfLight = (LIGHT_23 - LIGHT_13);
+                    var sectionLight = (currentLight - LIGHT_13);
+                    var percentageHeight = (33.3 + ((33.3) * ((1 - (sectionLight / totalOfLight))))).toFixed(0);
                 }
-                else if (currentLight <= LIGHT_23) {
-                    state = 'img/moon.svg';
+                else if (currentLight <= LIGHT_13) {
+                    var state = 'img/moon.svg';
+
+                    var totalOfLight = LIGHT_13;
+                    var percentageHeight = (66.6 + ((33.3) * ((1 - (currentLight / totalOfLight))))).toFixed(0);
+                    
+                    if (percentageHeight >= 80) {
+                        percentageHeight = 80;
+                    }
                 }
 
                 $('.star').css({
